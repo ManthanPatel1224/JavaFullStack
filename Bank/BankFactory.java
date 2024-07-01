cae1d6943231302fc2a8bffa4f895d0ab1c5b21c
Manthan Patel <105436800+ManthanPatel1224@users.noreply.github.com>
2024-07-01 14:51:27 +0530

    Delete Bank directory

--- Bank/BankFactory.java
+++
@@ -1,7 +0,0 @@
-package manthan.Bank;
-
-public abstract class BankFactory {
-
-	public abstract SavingAcc getNewSavingAcc(int accNo, String accNm, float accBal, boolean isSalaried);
-    public abstract CurrentAcc getNewCurrentAcc(int accNo, String accNm, float accBal, float creditLimit);
-}
